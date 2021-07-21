0f233816b04c959d6e989ce686f9266079ba117f
xzxin <xiazhenxin123@126.com>
2021-07-21 23:18:01 +0800

    增加创建商品功能

---
+++ src/main/java/com/miaoshaproject/controller/viewObject/ItemVO.java
@@ -0,0 +1,19 @@
+package com.miaoshaproject.controller.viewObject;
+
+import java.math.BigDecimal;
+
+public class ItemVO {
+    private Integer id;
+
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer stock;
+
+    private String description;
+
+    private Integer sales;
+
+    private String imgUrl;
+}
---
+++ src/main/java/com/miaoshaproject/service/impl/ItemServiceImpl.java
@@ -0,0 +1,101 @@
+package com.miaoshaproject.service.impl;
+
+import com.miaoshaproject.dao.ItemDOMapper;
+import com.miaoshaproject.dao.ItemStockDOMapper;
+import com.miaoshaproject.dataobject.ItemDO;
+import com.miaoshaproject.dataobject.ItemStockDO;
+import com.miaoshaproject.error.BussinessException;
+import com.miaoshaproject.error.EnumBussinessError;
+import com.miaoshaproject.service.ItemService;
+import com.miaoshaproject.service.model.ItemModel;
+import com.miaoshaproject.validator.ValidationResult;
+import com.miaoshaproject.validator.ValidatorImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    private ValidatorImpl validator;
+
+    @Autowired
+    private ItemDOMapper itemDOMapper;
+
+    @Autowired
+    private ItemStockDOMapper itemStockDOMapper;
+
+    private ItemDO convertItemDOfromItemModel(ItemModel itemModel) {
+        if (itemModel == null) {
+            return null;
+        }
+        ItemDO itemDO = new ItemDO();
+        BeanUtils.copyProperties(itemModel, itemDO);
+        itemDO.setPrice(itemModel.getPrice().doubleValue());
+        return itemDO;
+    }
+
+    private ItemStockDO convertItemStockDOFromItemModel(ItemModel itemModel) {
+        if (itemModel == null) {
+            return null;
+        }
+        ItemStockDO itemStockDO = new ItemStockDO();
+        itemStockDO.setItemId(itemModel.getId());
+        itemStockDO.setStock(itemModel.getStock());
+        return itemStockDO;
+    }
+
+    @Override
+    @Transactional
+    public ItemModel createItem(ItemModel itemModel) throws BussinessException {
+        // 校验入参
+        ValidationResult validate = validator.validate(itemModel);
+        if (validate.isHasErrors()) {
+            throw new BussinessException(
+                    EnumBussinessError.PARAMETER_VALIDATION_ERROR, validate.getErrorMsg());
+        }
+
+        // 转化itemMOdel - dataObject
+        ItemDO itemDO = this.convertItemDOfromItemModel(itemModel);
+
+        // 写入数据库
+        itemDOMapper.insertSelective(itemDO);
+        itemDO.setId(itemDO.getId());
+
+        ItemStockDO itemStockDO = this.convertItemStockDOFromItemModel(itemModel);
+        itemStockDOMapper.insertSelective(itemStockDO);
+        // 返回创建完成的对象
+
+        return this.getItemById(itemModel.getId());
+    }
+
+    @Override
+    public List<ItemModel> listItem() {
+        return null;
+    }
+
+    @Override
+    public ItemModel getItemById(Integer id) {
+        ItemDO itemDO = itemDOMapper.selectByPrimaryKey(id);
+        if (itemDO == null) {
+            return null;
+        }
+        ItemStockDO itemStockDO = itemStockDOMapper.selectByItemId(itemDO.getId());
+
+        // 讲dataobject -> model
+        ItemModel itemModel = convertModelFromDataObject(itemDO, itemStockDO);
+        return itemModel;
+    }
+
+    private ItemModel convertModelFromDataObject(ItemDO itemDO, ItemStockDO itemStockDO) {
+        ItemModel itemModel = new ItemModel();
+        BeanUtils.copyProperties(itemDO, itemModel);
+        itemModel.setPrice(new BigDecimal(itemDO.getPrice()));
+        itemModel.setStock(itemStockDO.getStock());
+        return itemModel;
+    }
+}
