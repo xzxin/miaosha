02c6921eb0c883cd072ba9de6950a864a994d605
xzxin <xiazhenxin123@126.com>
2021-08-01 20:19:52 +0800

    全局错误处理类优化

--- src/main/java/com/miaoshaproject/controller/BaseController.java
+++ src/main/java/com/miaoshaproject/controller/BaseController.java
@@ -15,19 +15,19 @@ import java.util.Map;
 public class BaseController {
     public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception exception) {
-        Map<String, Object> responseData = new HashMap<>();
-        if (!(exception instanceof BussinessException)) {
-            responseData.put("errCode", EnumBussinessError.UNKNOWN_ERROR.getErrCode());
-            responseData.put("errMsg", EnumBussinessError.UNKNOWN_ERROR.getErrMsg());
-        } else {
-            BussinessException bussinessException = (BussinessException) exception;
-            responseData.put("errCode", bussinessException.getErrCode());
-            responseData.put("errMsg", bussinessException.getErrMsg());
-        }
-        return CommonReturnType.create(responseData, "fail");
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Object handlerException(HttpServletRequest request, Exception exception) {
+//        Map<String, Object> responseData = new HashMap<>();
+//        if (!(exception instanceof BussinessException)) {
+//            responseData.put("errCode", EnumBussinessError.UNKNOWN_ERROR.getErrCode());
+//            responseData.put("errMsg", EnumBussinessError.UNKNOWN_ERROR.getErrMsg());
+//        } else {
+//            BussinessException bussinessException = (BussinessException) exception;
+//            responseData.put("errCode", bussinessException.getErrCode());
+//            responseData.put("errMsg", bussinessException.getErrMsg());
+//        }
+//        return CommonReturnType.create(responseData, "fail");
+//    }
 }
